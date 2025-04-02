23c6d8ed28cf48f813adb04081279b0e74f2f16b
cnfranzen <157517460+cnfranzen@users.noreply.github.com>
2025-04-02 07:31:18 -0600

    Update code

--- ActiveTestingRegressionMV.py
+++ ActiveTestingRegressionMV.py
@@ -1,27 +1,43 @@
-# Scikit-learn v1.4; Numpy v1.26.4; Pandas v2.2.1; Matplotlib v3.9
-# Keras v2.15; TF v2.15; TFP v0.23; KT v1.4.7
-
 import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt 
-from keras.models import Sequential  
+import pandas as pd
+import keras
+import matplotlib.pyplot as plt
+from keras.models import Sequential
 from keras.layers import Dense
-import tensorflow as tf  
-import tensorflow_probability as tfp  
-from keras.optimizers import RMSprop
-import time
-import keras_tuner  
 from keras.layers import Lambda
 from keras import backend as K
+import tensorflow as tf
+import tensorflow_probability as tfp
+from keras.optimizers import RMSprop
+import time
+
+import keras_tuner
 
 tfd = tfp.distributions
 tfpl = tfp.layers
 
 def PermaDropout(rate):
-   return Lambda(lambda x: K.dropout(x, level=rate))
+    return Lambda(lambda x: K.dropout(x, level=rate))
 
 def quadratic_loss(y, y_hat): 
     return np.power((y - y_hat), 2)
+
+def negloglik(y_true, y_pred):
+    return -y_pred.log_prob(y_true)
+
+def nll(y_true, y_pred):
+    """
+    Description: Compute Negative log likelihood by calculating the log_probabilities of our training data 
+    and returning a tensor the same shape as x_train.  Computes the mean of the log probabilities 
+    so loss does not scale with the size of the dataset.
+
+    Args: (x_train, distribution) 
+    x_train is the training data set
+    type of distribution for computing log probs
+
+    Returns: Negative log likelihood
+    """
+    return -tf.reduce_mean(y_pred.log_prob(y_true))
 
 def find_mean_var_model(X_train, y_train, X_test, reg_evaluator, n_reps = 30, model_type='boot'):
     
@@ -29,32 +45,42 @@ def find_mean_var_model(X_train, y_train, X_test, reg_evaluator, n_reps = 30, mo
 
     if model_type == 'refit':
         for i in range(n_reps):
-            reg_evaluator.fit(X_train, y_train, epochs=1, batch_size=10, verbose=0)
+            reg_evaluator.fit(X_train, y_train, epochs = 1, batch_size=10, verbose=False)
             y_pred[:,i] = reg_evaluator.predict(X_test).ravel()
+        
+        meanVec = y_pred.mean(axis=1)
+        varVec = y_pred.var(axis=1)
 
     if model_type == 'boot':
         for i in range(n_reps):
             ind = np.random.choice(len(X_train), len(X_train), replace=True) # Randomly choose index number for X number of samples with replacement
             X_pert = X_train[ind] # Input values from training features from matching index
             y_pert = y_train[ind] # Input values from training responses from matching index
-            reg_evaluator.fit(X_pert, y_pert) # fit model to bootstrapped data
+            reg_evaluator.fit(X_pert, y_pert, epochs = 20, verbose=False) # fit model to bootstrapped data
             y_pred[:,i] = reg_evaluator.predict(X_test).ravel() # Collect all bootstraps in one matrix
+        
+        meanVec = y_pred.mean(axis=1)
+        varVec = y_pred.var(axis=1)
     
     if model_type == 'drop':
+        reg_evaluator.fit(X_train, y_train, epochs=10, batch_size=10, verbose=False)
         for i in range(n_reps):
-            reg_evaluator.fit(X_train, y_train, epochs=1, batch_size=10, verbose=0)
             y_pred[:,i] = reg_evaluator.predict(X_test).ravel()
+
+        meanVec = y_pred.mean(axis=1)
+        varVec = y_pred.var(axis=1)
 
     if model_type == 'prob':
-        for i in range(n_reps):
-            y_pred[:,i] = reg_evaluator.predict(X_test).ravel()
+        y_pred = reg_evaluator(X_test)
+
+        meanVec = y_pred.mean().numpy().squeeze()
+        varVec = y_pred.stddev().numpy().squeeze()
 
     if model_type == 'vi':
-        for i in range(n_reps):
-            y_pred[:,i] = reg_evaluator.predict(X_test).ravel()
+        y_pred = reg_evaluator(X_test)
 
-    meanVec = y_pred.mean(axis=1)
-    varVec = y_pred.var(axis=1)
+        meanVec = y_pred.mean().numpy().squeeze()
+        varVec = y_pred.stddev().numpy().squeeze()   
     
     return meanVec, varVec
 
@@ -66,10 +92,6 @@ def surrogate_sampling_model(X_train, y_train, X_test, y_pred, M, reg_evaluator,
     remaining_idx = np.arange(N, dtype=int) # Create matrix to track the indices for N samples by initializing with 0-(N-1) indices 
     observed_idx = np.array([], dtype=int) # Initialize empty array with integer data type to capture desired test points
     weights = np.array([]) # Initialize empty array to capture weights based on index
-
-    if model_type == 'refit':
-        # find the mean and variance associated with the evaluator 
-        meanVec, varVec = find_mean_var_model(X_train, y_train, X_test, reg_evaluator, n_reps = n_reps, model_type='refit')
 
     if model_type == 'boot':
         # find the mean and variance associated with the evaluator 
@@ -97,7 +119,7 @@ def surrogate_sampling_model(X_train, y_train, X_test, y_pred, M, reg_evaluator,
         
     return observed_idx, weights # output M selected test points and their associated probability
 
-def run_exp(X_train, y_train, X_test, y_test, y_pred, reg_evaluator, num_exp = 30, n_reps = 100, model_type="boot", plot_title="Bootstrap"):
+def run_exp(X_train, y_train, X_test, y_test, y_pred, reg_evaluator, num_exp = 30, n_reps = 30, model_type="boot", show_legend="False"):
     N = len(X_test)
     M_values = np.array([0.1, 0.2, 0.3, 0.4, 0.5]) * N
     M_values = M_values.astype(int)
@@ -142,11 +164,12 @@ def run_exp(X_train, y_train, X_test, y_test, y_pred, reg_evaluator, num_exp = 3
     ax.set_xticks(M_values)
     ax.set_ylabel('error estimate (mean+/-std)', fontsize=19)
     ax.set_xlabel('test data size $M$', fontsize=19)
-    ax.legend(labels=['test error $R$', 'uniform', 'surrogate, unweighted',  'surrogate, weighted'], ncol=2, fontsize=14, framealpha=0.5)
-    plt.title(f'{plot_title} Surrogate Sampling')
+    ax.set_ylim(0,1.5)
+    if show_legend == "True":
+        ax.legend(labels=['test error $R$', 'uniform', 'surrogate, unweighted',  'surrogate, weighted'], ncol=2, fontsize=14, framealpha=0.5)
     plt.show()
 
-def run_exp_weighted(X_train, y_train, X_test, y_test, y_pred, reg_evaluator, num_exp = 30, n_reps = 100, model_type="boot", plot_title="Bootstrap"):
+def run_exp_weighted(X_train, y_train, X_test, y_test, y_pred, reg_evaluator, num_exp = 30, n_reps = 30, model_type="boot", show_legend="False", exp_type = "ish"):
     N = len(X_test)
     M_values = np.array([0.1, 0.2, 0.3, 0.4, 0.5]) * N
     M_values = M_values.astype(int)
@@ -186,8 +209,14 @@ def run_exp_weighted(X_train, y_train, X_test, y_test, y_pred, reg_evaluator, nu
     ax.set_xticks(M_values)
     ax.set_ylabel('error estimate (mean+/-std)', fontsize=19)
     ax.set_xlabel('test data size $M$', fontsize=19)
-    ax.legend(labels=['test error $R$', 'uniform', 'surrogate, unweighted',  'surrogate, weighted'], ncol=2, fontsize=14, framealpha=0.5)
-    plt.title(f'{plot_title} Surrogate Sampling')
+    if exp_type == "ish":
+        ax.set_ylim(0.5,3)
+    if exp_type == "conc":
+        ax.set_ylim(8,13)
+    if exp_type == "eeb":
+        ax.set_ylim(2,2.8)
+    if show_legend == "True":
+        ax.legend(labels=['test error $R$', 'uniform', 'surrogate, unweighted',  'surrogate, weighted'], ncol=2, fontsize=14, framealpha=0.5)
     plt.show()
 
     # Print results  
@@ -198,29 +227,36 @@ def run_exp_weighted(X_train, y_train, X_test, y_test, y_pred, reg_evaluator, nu
     display(df) #type: ignore
     print("Full Test R Value:", full_loss)
 
-def time_weight(X_train, y_train, X_test, y_test, y_pred, reg_evaluator, num_exp = 30, n_reps = 100, model_type="boot", plot_title="Bootstrap"):
+def time_weight(X_train, y_train, X_test, y_test, y_pred, reg_evaluator, num_exp = 30, n_reps = 30, model_type="boot"):
     N = len(X_test)    
     M_values = np.array([0.1, 0.2, 0.3, 0.4, 0.5]) * N
     M_values = M_values.astype(int)
     result_mod_weighted = np.zeros((num_exp, len(M_values))) # nonuniform, weighted (unbiased)
     timemat = []
 
-    for i in range(len(M_values)): 
-        start = time.time()   
+    for i in range(len(M_values)):    
         M = M_values[i]
         print('---' + str(M) + '---')
         
+        start = time.time()
+        
         for exp in range(num_exp):
-            # nonuniform, weighted
+            # uniform 
+            observed_idx = np.random.choice(N, M, replace=False)
+            weights = np.ones(M)/M 
+           
+            # nonuniform, unweighted 
             observed_idx, weights = surrogate_sampling_model(X_train, y_train,
                                             X_test, y_pred, M, reg_evaluator, n_reps = n_reps, loss=quadratic_loss, model_type=model_type)
-    
+
+            # nonuniform, weighted 
             result_mod_weighted[exp, i] = np.sqrt(risk_estimator_partial(y_test[observed_idx], y_pred[observed_idx], N,  
                         weights, quadratic_loss, mode='weighted'))
+        
         end = time.time()    
         timemat = np.append(timemat, end - start)
 
-    print(timemat)
+    return timemat
 
 def risk_estimator_partial(y_true, y_pred, N,  acq_weights, loss_function, mode='unweighted'):
     '''
@@ -278,8 +314,8 @@ def create_model(X_train, y_train, model_type, min_layers, max_layers, min_value
             objective = "mean_squared_error",
             max_epochs = 10,
             factor = 3,
-            directory = "tune_dir",
-            project_name = "boot")
+            overwrite = True,
+            directory = "boot_mod")
             
         tuner.search(X_train, y_train, epochs=5)
 
@@ -296,14 +332,14 @@ def create_model(X_train, y_train, model_type, min_layers, max_layers, min_value
                 activation = 'relu', 
                 input_shape = (X_train.shape[1],) 
             ))
-            model.add(PermaDropout(0.5))
+            model.add(PermaDropout(rate = 0.5))
             
             for ind in range(hp.Int('num_layers', min_layers, max_layers)):
                 model.add(Dense(
                     units = hp.Int("units_" + str(ind), min_value=min_value, max_value=max_value, step=step),
                     activation = "relu"
                 ))
-                model.add(PermaDropout(0.5))
+                model.add(PermaDropout(rate = 0.5))
             
             # Add the output layer with the specified output shape 
             model.add(Dense(1))
@@ -317,8 +353,8 @@ def create_model(X_train, y_train, model_type, min_layers, max_layers, min_value
             objective = "mean_squared_error",
             max_epochs = 10,
             factor = 3,
-            directory = "tune_dir",
-            project_name = "drop")
+            overwrite = True,
+            directory = "drop_mod")
             
         tuner.search(X_train, y_train, epochs=5)
 
@@ -346,7 +382,7 @@ def create_model(X_train, y_train, model_type, min_layers, max_layers, min_value
             model.add(Dense(tfpl.IndependentNormal.params_size(event_shape=1)))
             model.add(tfpl.IndependentNormal(event_shape=1))
 
-            model.compile(optimizer='adam', loss='mse', metrics=['mean_squared_error'])
+            model.compile(optimizer='adam', loss=nll, metrics=['mean_squared_error'])
 
             return model
 
@@ -355,9 +391,9 @@ def create_model(X_train, y_train, model_type, min_layers, max_layers, min_value
             objective = "mean_squared_error",
             max_epochs = 10,
             factor = 3,
-            directory = "tune_dir",
-            project_name = "prob")
-        
+            overwrite = True,
+            directory = "prob_mod")
+            
         tuner.search(X_train, y_train, epochs=5)
 
         best_model = tuner.get_best_hyperparameters(5)  # Pull best hp from 
@@ -366,11 +402,16 @@ def create_model(X_train, y_train, model_type, min_layers, max_layers, min_value
 
     return reg_evaluator
 
-def plot_model(X_train, y_train, reg_evaluator):
+def plot_model(X_train, y_train, reg_evaluator, n_epochs=1000):
+    train_start = time.time()
+
     history = reg_evaluator.fit(X_train, y_train,
                 batch_size=10,
-                epochs=50,
+                epochs=n_epochs,
                 verbose=0)
+
+    train_end = time.time()
+    train_time = train_end - train_start
 
     # Summary of the model
     reg_evaluator.summary()
@@ -388,8 +429,10 @@ def plot_model(X_train, y_train, reg_evaluator):
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
     plt.show()
+
+    return train_time
     
-def plot_1D(X_train, y_train, X_test, X_new, reg_evaluator, reg_learner, n_reps=30, model_type="boot", plot_type = "Bootstrap"):
+def plot_1D(X_train, y_train, X_test, X_new, reg_evaluator, reg_learner, n_reps=30, model_type="boot", show_legend="False"):
     mean, var = find_mean_var_model(X_train, y_train, X_test, reg_evaluator, n_reps = n_reps, model_type = model_type)
 
     # Plot Bootstrap Results
@@ -398,12 +441,12 @@ def plot_1D(X_train, y_train, X_test, X_new, reg_evaluator, reg_learner, n_reps=
     plt.plot(X_train, y_train, "C5o", markersize=5, label='$D_{train}$')
     plt.plot(X_new, reg_learner.predict(X_new[:,np.newaxis]), 'k:', label="trained $f$")
     plt.errorbar(X_test, mean, yerr=var, ls='none', marker='s', c="C9",
-                markersize=4, capsize=2, label= 'results for $\mathcal{X}_{test}$ surrogate')
+                markersize=4, capsize=2, label= 'results for $\mathcal{X}_{test}$\nsurrogate')
     plt.xlabel("$x$")
     plt.ylabel("$y$", rotation=0)
-    plt.legend(fontsize=13)
+    if show_legend == "True":
+        plt.legend(fontsize=13)
     plt.ylim([-3.2,1.5])
-    plt.title(f'{plot_type} Surrogate')
     plt.show()
 
 
@@ -441,15 +484,31 @@ def posterior_mean_field(kernel_size, bias_size=0, dtype=None):
         tfp.layers.VariableLayer(2 * n, dtype=dtype),
         tfp.layers.DistributionLambda(lambda t: tfd.Independent(
             tfd.Normal(loc=t[..., :n],
-                        scale=1e-5 + tf.nn.softplus(c + t[..., n:])),
+                        scale=1e-3 + tf.nn.softplus(c + 0.1 * t[..., n:])),
             reinterpreted_batch_ndims=1)),
     ])
 
-negloglik = lambda y, rv_y: -rv_y.log_prob(y)
+def build_vi(X_train, y_train, exp_type = "eeb"):
+    if exp_type == "1D":
+        coef = 0.0005
+        offset = 1e-3
+        n_epochs = 8000
+    if exp_type == "ish":
+        coef = 0.0005 
+        offset = 1e-3
+        n_epochs = 8000 
+    if exp_type == "conc":
+        coef = 0.1 
+        offset = 1e-2
+        n_epochs = 1250
+    if exp_type == "eeb":
+        coef = 0.0005
+        offset = 1e-3
+        n_epochs = 4000
 
-def build_vi(X_train, y_train):
     reg_evaluator = Sequential([
-    tfpl.DenseVariational(units=2,
+    tfpl.DenseVariational(input_shape=(X_train.shape[1],),
+                          units=2,
                           make_prior_fn = prior_trainable,
                           make_posterior_fn = posterior_mean_field,
                           kl_weight=1/X_train.shape[0], 
@@ -460,12 +519,17 @@ def build_vi(X_train, y_train):
                           kl_weight=1/X_train.shape[0]),
     tfpl.DistributionLambda(
         lambda t: tfd.Normal(loc=t[..., :1],
-                            scale=1e-3 + tf.math.softplus(0.01 * t[...,1:]))),
+                            scale=offset + tf.math.softplus(coef * t[...,1:]))),
     ])
 
     reg_evaluator.compile(loss=negloglik, optimizer=RMSprop(learning_rate=0.01))
-    reg_evaluator.fit(X_train, y_train, epochs=10000, verbose=False)
+
+    start = time.time()
+    reg_evaluator.fit(X_train, y_train, epochs=n_epochs, verbose=False)
+    end = time.time()
+    train_time = end - start
+    
     reg_evaluator.summary()
 
-    return reg_evaluator
+    return reg_evaluator, train_time
     
